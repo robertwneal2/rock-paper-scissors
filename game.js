14a696b81f628cfd063237d1c961ab488c840637
Robert Neal <robertwneal2@gmail.com>
2022-07-08 11:44:21 -0700

    Add game function. Finish game

--- game.js
+++ game.js
@@ -41,6 +41,50 @@ function capitalize(str) {
     return (firstLetter + restOfStr);
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    console.log('Start game:')
+    let computerSelection = ''
+    let playerSelection = ''
+    for (let i = 0; i < 5; i++) {
+        console.log(`Round ${i+1}:`)
+        computerSelection = computerPlay();
+        playerSelection = playerSelect();
+        if (playerSelection === 'Cancel') {
+            console.log('Game canceled!')
+            break
+        }
+        console.log(`You chose: ${playerSelection}`)
+        console.log(`Computer chose: ${computerSelection}`)
+        console.log(playRound(playerSelection, computerSelection));
+     }
+    console.log('Game over!')
+}
+
+function playerSelect() {
+    let playerSelection = 0;
+    let playerMove = '';
+    let loopDone = false;
+    while (loopDone === false) {
+        playerInput = prompt('Enter 1 for Rock, 2 for Paper, 3 for Rock: ');
+        if (playerInput === null) { //cancel game when cancel pressed. 
+            playerMove = 'Cancel'
+            break
+        }
+        playerSelection = parseInt(playerInput);
+        if (playerSelection === 1) {
+            playerMove = 'Rock';
+            loopDone = true;
+        } else if (playerSelection === 2) {
+            playerMove = 'Paper';
+            loopDone = true;
+        } else if (playerSelection === 3) {
+            playerMove = 'Scissors';
+            loopDone = true;
+        } else { //make user enter 1, 2, or 3
+            console.log('Must enter 1, 2, or 3')
+        }
+    }
+    return playerMove
+}
+
+game()
